623b11d67b54888aca6586d33c81eb2ac245dc0d
nothing628 <yogyphang@gmail.com>
2026-03-06 23:22:03 +0700

    fix sass deprecation

--- src/app/globals.d.ts
+++ src/app/globals.d.ts
@@ -1,103 +1,102 @@
 interface Window {
-    [key: string]: any;
-    xbreader: any;
-    // xbconfig: XBConfig; DEPRECATED
+  [key: string]: any;
+  xbreader: any;
+  // xbconfig: XBConfig; DEPRECATED
 }
 
 interface MithrilEvent extends Event {
-    redraw: boolean;
-    special: boolean;
+  redraw: boolean;
+  special: boolean;
 }
 
 interface XBTab {
-    title: string;
-    href: string;
-    icon: string;
-    target?: string;
-    prefix: boolean;
+  title: string;
+  href: string;
+  icon: string;
+  target?: string;
+  prefix: boolean;
 }
 
 interface XBBrand {
-    name: string;
-    logo: string;
-    titled: boolean;
-    embedded: boolean;
-    href: string;
+  name: string;
+  logo: string;
+  titled: boolean;
+  embedded: boolean;
+  href: string;
 }
 
 interface XBChapter {
-    no: number;
-    title: string;
-    uuid: string;
-    selected: boolean;
+  no: number;
+  title: string;
+  uuid: string;
+  selected: boolean;
 }
 
 interface XBVolume {
-    no: number;
-    title: string;
-    uuid: string;
-    chapters: XBChapter[];
+  no: number;
+  title: string;
+  uuid: string;
+  chapters: XBChapter[];
 }
 
 interface XBOption {
-    label: string | number;
-    description?: string;
-    value: string | number;
+  label: string | number;
+  description?: string;
+  value: string | number;
 }
 
 interface XBSetting {
-    title: string;
-    description?: string;
-    reflowable: boolean; // TODO better way of separating the settings, maybe an array?
-    name: string;
-    value: string | number;
-    type: number;
-    options: XBOption[];
+  title: string;
+  description?: string;
+  reflowable: boolean; // TODO better way of separating the settings, maybe an array?
+  name: string;
+  value: string | number;
+  type: number;
+  options: XBOption[];
 }
 
 interface XBRenderConfig {
-    bitmap: boolean;
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    onDraw: Function;
-    lok: boolean;
+  bitmap: boolean;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  onDraw: Function;
+  lok: boolean;
 }
 
 interface XBConfig {
-    //[key: string]: any;
-    settings: XBSetting[];
-    brand: XBBrand;
-    tabs: XBTab[];
-    prefix: string;
-    mount: HTMLElement;
-    guideHidden: boolean;
-    // preview: boolean;
-    link: string;
-    series: XBVolume[];
-    loader(identifier: string): Record<string, unknown>; // TODO Object -> WebPub
+  //[key: string]: any;
+  settings: XBSetting[];
+  brand: XBBrand;
+  tabs: XBTab[];
+  prefix: string;
+  mount: HTMLElement;
+  guideHidden: boolean;
+  // preview: boolean;
+  link: string;
+  series: XBVolume[];
+  loader(identifier: string): Record<string, unknown>; // TODO Object -> WebPub
 
-    // Reader callbacks
-    onMount(reader: any): void;
-    onInit(reader: any): void;
-    onPublicationLoad(reader: any): void;
-    onBeforeReady(reader: any): void;
-    onReady(reader: any): void;
-    onPageChange(pnum: number, direction: string, isSpread: boolean): void;
-    onLastPage(series: any, pnum: number): boolean; // TODO Series type
-    onToggleInterface(): void;
+  // Reader callbacks
+  onMount(reader: any): void;
+  onInit(reader: any): void;
+  onPublicationLoad(reader: any): void;
+  onBeforeReady(reader: any): void;
+  onReady(reader: any): void;
+  onPageChange(pnum: number, direction: string, isSpread: boolean): void;
+  onLastPage(series: any, pnum: number): boolean; // TODO Series type
+  onToggleInterface(): void;
 
-    // Page callbacks
-    onSource(link: unknown): unknown;
-    onError(link: unknown): unknown; // TODO add reader or something
-    render: XBRenderConfig;
+  // Page callbacks
+  onSource(link: unknown): unknown;
+  onError(link: unknown): unknown; // TODO add reader or something
+  render: XBRenderConfig;
 
-    // Settings
-    additionalSettings: XBSetting[];
-    onSettingsSave(settings: Record<string, string | number>): boolean;
-    onSettingsLoad(version: string): Record<string, string>;
+  // Settings
+  additionalSettings: XBSetting[];
+  onSettingsSave(settings: Record<string, string | number>): boolean;
+  onSettingsLoad(version: string): Record<string, string>;
 }
 
 declare const __NAME__: string;
 declare const __VERSION__: string;
 
 /// WebPub
-
